cb008221b07c4cfda56bf93c5fd1f5846c278677
James Seymour <james.seymour@cubiko.com.au>
2022-04-05 09:36:28 +1000

    add base models for A2

--- Assignments/A2/solve.py
+++ Assignments/A2/solve.py
@@ -1,2 +1,69 @@
+from collections import defaultdict
+from typing import Generator, Tuple, Union
+from gurobipy import Model, GRB, TempConstr, Var, tupledict, quicksum, max_, min_
+from itertools import product, chain
+from vaccine2 import (
+    # Var definitions
+    weeks,
+    IDs,
+    LVCs,
+    CCDs,
 
-test
+    # Cost definitions
+    ID_costs,
+    IDtoLVC_costs,
+    CCDtoLVC_costs,
+    hello,
+
+    # Constraint definitions
+    CCD_pops,
+
+    # Communication constraint constants
+    COMM6_ID_MAX,
+    COMM6_LVC_MAX,
+)
+
+# Type defs
+
+Gurobi_DecisionVar_Dict = tupledict[Tuple[str, ...], Union[Var, int]]
+Gurobi_Constraint_Gen = Generator[TempConstr, None, None]
+
+## Setup model, variables and objective
+
+model = Model()
+
+ID_vars: Gurobi_DecisionVar_Dict = model.addVars(weeks, IDs) # Number of vaccines sent to each ID
+IDtoLVC_vars: Gurobi_DecisionVar_Dict = model.addVars(weeks, IDs, LVCs) # Number of
+# Use sparse variable creation here, as a 0 cost CCDtoLVC is inaccessible. Then wrap in a defaultdict to return 0 if key doesnt have val
+CCDtoLVC_vars: Gurobi_DecisionVar_Dict = defaultdict(int,
+    model.addVars(filter(lambda var_key: CCDtoLVC_costs[var_key] > 0, product(weeks, CCDs, LVCs)))
+) # Number of people at each CCD to be vaccinated at each LVC
+
+total_ID_cost = quicksum(ID_costs[var_key] * gurobi_var for var_key, gurobi_var in ID_vars.items())
+total_IDtoLVC_cost = quicksum(IDtoLVC_costs[var_key] * gurobi_var for var_key, gurobi_var in IDtoLVC_vars.items())
+total_CCDtoLVC_cost = quicksum(CCDtoLVC_costs[var_key] * gurobi_var for var_key, gurobi_var in CCDtoLVC_vars.items())
+
+total_cost = total_ID_cost + total_IDtoLVC_cost + total_CCDtoLVC_cost
+
+model.setObjective(total_cost, GRB.MINIMIZE)
+
+### Communication 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignments/A2/vaccine2.py
@@ -0,0 +1,55 @@
+IDtoLVC = [[43.3, 83.2, 7.8, 63.4, 60.3, 59.3, 44.7, 37.0],
+           [80.6, 64.5, 52.7, 77.2, 5.0, 41.5, 15.3, 58.1],
+           [42.4, 23.4, 61.9, 16.2, 71.6, 26.0, 61.8, 30.5]]
+
+CCDPop = [
+    4426, 6527, 6821, 5566, 6164, 5002, 6377, 4747, 5992, 3517, 4602, 4135,
+    4097, 4296, 5379, 4368, 4304, 4496, 5884, 4222, 5135, 4325, 3861, 5128,
+    5384
+]
+
+CCDtoLVC = [[0, 0, 17.7, 0, 0, 0, 0, 48.9], [0, 0, 11.6, 0, 0, 0, 29.0, 0],
+            [0, 0, 22.2, 0, 0, 0, 21.2, 0], [0, 0, 0, 0, 11.0, 0, 9.2, 0],
+            [0, 0, 0, 0, 10.9, 0, 24.2, 0], [37.5, 0, 22.7, 0, 0, 0, 0, 0],
+            [0, 0, 8.0, 0, 0, 0, 0, 25.7],
+            [0, 0, 24.1, 0, 0, 29.3, 21.9, 25.0],
+            [0, 0, 0, 0, 20.8, 25.9, 14.4, 0], [0, 0, 0, 0, 17.8, 0, 27.2, 0],
+            [10.8, 0, 0, 0, 0, 0, 0, 23.2], [15.2, 0, 0, 28.9, 0, 0, 0, 8.9],
+            [0, 0, 0, 0, 0, 12.4, 0, 23.0], [0, 0, 0, 0, 0, 12.9, 33.0, 0],
+            [0, 0, 0, 0, 35.3, 21.0, 0, 0], [5.4, 0, 0, 0, 0, 0, 0, 28.7],
+            [24.2, 0, 0, 17.4, 0, 30.0, 0, 10.3],
+            [0, 27.2, 0, 20.5, 0, 18.3, 0, 22.5],
+            [0, 11.5, 0, 30.9, 0, 17.9, 0, 0], [0, 16.3, 0, 0, 0, 23.9, 0, 0],
+            [23.0, 0, 0, 19.3, 0, 0, 0, 0], [0, 0, 0, 6.2, 0, 0, 0, 33.5],
+            [0, 27.8, 0, 20.5, 0, 0, 0, 0], [0, 9.5, 0, 0, 0, 36.2, 0, 0],
+            [0, 14.4, 0, 0, 0, 35.6, 0, 0]]
+LVCUCost = [
+    1208000, 1770000, 1954000, 1171000, 1601000, 1736000, 1604000, 1136000
+]
+
+ID_import_cost_per_dose = [109, 160, 168]
+IDtoLVC_cost_per_dose = [[0.2 * distance for distance in ID] for ID in IDtoLVC]
+CCDtoLVC_cost_per_dose = CCDtoLVC
+
+# Exports
+
+weeks = [f"WEEK{i}" for i in range(6)]
+IDs = ["ID-A", "ID-B", "ID-C"]
+LVCs = [f"LVC{i}" for i in range(8)]
+CCDs = [f"CCD{i}" for i in range(25)]
+
+ID_costs = {(week, ID): ID_import_cost_per_dose[ID_i]
+            for week in weeks for ID_i, ID in enumerate(IDs)}
+
+IDtoLVC_costs = {(week, ID, LVC): IDtoLVC_cost_per_dose[ID_i][LVC_i]
+                 for week in weeks for LVC_i, LVC in enumerate(LVCs)
+                 for ID_i, ID in enumerate(IDs)}
+
+CCDtoLVC_costs = {(week, CCD, LVC): CCDtoLVC_cost_per_dose[CCD_i][LVC_i]
+                  for week in weeks for LVC_i, LVC in enumerate(LVCs)
+                  for CCD_i, CCD in enumerate(CCDs)}
+
+CCD_pops = {CCD: CCDPop[CCD_i] for CCD_i, CCD in enumerate(CCDs)}
+
+COMM6_ID_MAX = 50000
+COMM6_LVC_MAX = 15000
